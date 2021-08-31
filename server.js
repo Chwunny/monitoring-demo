6cc3497e2813f1943b0a722d675e6373284a643d
Zach King <zacheking10@gmail.com>
2021-08-31 12:35:13 -0600

    added rollbar errors

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const students = []
 
 const app = express()
 app.use(express.json())
+app.use('/style', express.static(path.join(__dirname, '/public/styles.css')))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
@@ -27,6 +28,27 @@ app.post('/api/student', (req, res) => {
     rollbar.log('Student added successfully', {author: 'Zach', type: 'manual entry'})
     res.status(200).send(students)
 })
+
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    const index = students.findIndex(studentName=> studentName === name)
+
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added scuccessfully')
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.error('Student already exists')
+        res.status(400).send('that student already exists')
+    }
+
+})
+
 
 const port = process.env.PORT || 4545
 
